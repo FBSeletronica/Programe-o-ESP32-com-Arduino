6c5d4356c957946508d9983c98a1531fb0624811
Fábio Souza <fs.embarcados@gmail.com>
2023-09-28 15:28:16 -0300

    refatoraçaoi

--- dac/dac.ino
+++ dac/dac.ino
@@ -6,21 +6,21 @@
  * Data: 21/09/2023
 */
 
-int Sin_Array[256];                 // vetor para os valores da senoide
-float Period = (2*PI)/256;          // período da onda
-float Rad_Angle;                    // variável para o ângulo em radianos
+int sine_wave[256];                 // vetor para os valores da senoide
+float period = (2*PI)/256;          // período da onda
+float angle_rad;                    // variável para o ângulo em radianos
 
 void setup(){
 
  //gera o vetor com os valores da senoide
- for(int Angle=0; Angle<256; Angle++) {
-   Rad_Angle = Angle*Period;                     // calcula o ângulo em radianos
-   Sin_Array[Angle] = (sin(Rad_Angle)*127)+128;  // calcula o valor da senoide e armazena no vetor faz um offset para que os valores fiquem entre 0 e 255
+ for(int i=0; i<255; i++) {
+   angle_rad = i*period;                     // calcula o ângulo em radianos
+   sine_wave[i] = (sin(angle_rad)*127)+128;  // calcula o valor da senoide e armazena no vetor faz um offset para que os valores fiquem entre 0 e 255
  }
 
 }
 
 void loop(){
- for(int i=0;i<256;i++)             // Loop para percorrer o vetor
-   dacWrite(17,Sin_Array[i]);       // Escreve o valor da senoide no DAC
+ for(int i=0;i<255;i++)             // Loop para percorrer o vetor
+   dacWrite(17,sine_wave[i]);       // Escreve o valor da senoide no DAC
 }
