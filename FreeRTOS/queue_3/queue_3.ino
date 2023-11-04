22fe8072e69cef14e7418c571f1b2b0d054613dc
Fábio Souza <fs.embarcados@gmail.com>
2023-11-04 08:03:50 -0300

    ajuste na mensagem para estrutura

--- FreeRTOS/queue_3/queue_3.ino
+++ FreeRTOS/queue_3/queue_3.ino
@@ -27,33 +27,47 @@ const int LED =  LED_VERMELHO;    // Pino do LED
 // Definição da fila
 QueueHandle_t xQueue;
 
+//estrutura para mensagem
+typedef struct{
+    int id;             // ID da mensagem
+    char string[20];    // String da mensagem
+} Mensagem_t;
+
+// Função para enviar a mensagem
 void sender_task(void *pvParameters) {
-    int message = 1;
+    Mensagem_t mensagem;
+    mensagem.id = 1;                                // Inicializa o ID da mensagem
+        
     while (1) 
     {
-        xQueueSend(xQueue, &message, portMAX_DELAY);        // Envia a mensagem para a fila
-        vTaskDelay(pdMS_TO_TICKS(1000));                    // Delay de 1 segundo
-        message++;                                          // Altera a mensagem
+        snprintf(mensagem.string, sizeof(mensagem.string), "Mensagem %d", mensagem.id);   // Cria a string da mensagem
+        xQueueSend(xQueue, &mensagem, portMAX_DELAY);                                     // Envia a mensagem para a fila
+        mensagem.id++;                                                                   // Incrementa o ID da mensagem
+        vTaskDelay(pdMS_TO_TICKS(1000));                                                 // Delay de 1 segundo
+        
     }
 }
 
+// Função para receber a mensagem
 void receiver_task1(void *pvParameters) {
-    int received_message;                                    // Variável que irá armazenar a mensagem recebida
+    Mensagem_t mensagem_peek;                                                         // Variável que irá armazenar a mensagem recebida
     while (1) 
     {
-        if (xQueuePeek(xQueue, &received_message, 0) == pdTRUE) {                   // xQueuePeek não remove a mensagem da fila
-            Serial.printf("Task 1 recebeu a mensagem: %d\n", received_message);     // Imprime a mensagem
-        }
-        vTaskDelay(pdMS_TO_TICKS(500));                                             // Atraso de 0,5 segundos
+        if (xQueuePeek(xQueue, &mensagem_peek, 0) == pdTRUE) {                                               // xQueuePeek não remove a mensagem da fila
+           Serial.printf("Task 1 Peek a mensagem ID %d: %s\n", mensagem_peek.id, mensagem_peek.string);   // Imprime a mensagem
+        }        
+        vTaskDelay(pdMS_TO_TICKS(250));                                             // Atraso de 0,5 segundos                                                                                            // A mensagem continua na fila                                                              // Atraso de 0,5 segundos
     }
 }
 
+// Função para receber a mensagem
 void receiver_task2(void *pvParameters) {
-    int received_message;
+    Mensagem_t mensagem_recebida;
+    
     while (1) 
     {
-        if (xQueueReceive(xQueue, &received_message, portMAX_DELAY) == pdPASS) {    // xQueueReceive remove a mensagem da fila
-            Serial.printf("Task 2 recebeu a mensagem: %d\n", received_message);     // Imprime a mensagem
+        if (xQueueReceive(xQueue, &mensagem_recebida, portMAX_DELAY) == pdPASS) {    // xQueueReceive remove a mensagem da fila
+            Serial.printf("Task 2 retirou a mensagem ID %d: %s\n", mensagem_recebida.id, mensagem_recebida.string);   // Imprime a mensagem
         }
         vTaskDelay(pdMS_TO_TICKS(500));                                             // Atraso de 0,5 segundos
     }
@@ -62,11 +76,11 @@ void receiver_task2(void *pvParameters) {
 void setup() {
     Serial.begin(9600);                                             // Inicializa a Serial
     pinMode(LED,OUTPUT);                                            // Configura o LED como saída
-    xQueue = xQueueCreate(QUEUE_LENGTH, sizeof(int));
+    xQueue = xQueueCreate(QUEUE_LENGTH, sizeof(Mensagem_t));        // Cria a fila com 2 elementos do tipo Mensagem_t
 
-    xTaskCreate(sender_task, "Sender Task", 2048, NULL, 2, NULL);
-    xTaskCreate(receiver_task1, "Receiver Task 1", 2048, NULL, 1, NULL);
-    xTaskCreate(receiver_task2, "Receiver Task 2", 2048, NULL, 1, NULL);
+    xTaskCreate(sender_task, "Sender Task", 2048, NULL, 2, NULL);           // Cria a task Sender
+    xTaskCreate(receiver_task1, "Receiver Task 1", 2048, NULL, 1, NULL);    // Cria a task Receiver
+    xTaskCreate(receiver_task2, "Receiver Task 2", 2048, NULL, 1, NULL);    // Cria a task Receiver
 }
 
 void loop(){
