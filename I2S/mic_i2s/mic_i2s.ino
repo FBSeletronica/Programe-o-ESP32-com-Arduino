2c507cb10650028933d322f028b9ab08ed4fcdae
FBSeletronica <fs.embarcados@gmail.com>
2025-02-28 22:08:38 -0300

    Create mic_i2s.ino

---
+++ I2S/mic_i2s/mic_i2s.ino
@@ -0,0 +1,62 @@
+/*
+ * Exemplo com de I2s para leitura do microfone INMP441 usando a biblioteca ESP_I2S
+ * 
+ * descrição: Este código é um exemplo de utilização do microfone INMP441 com o ESP32 
+ * utilizando a bilbioteca ESP_I2S
+ * objetivo: Entender o funcionamento bãsico da interface I2S
+ *  
+ * Autor: Fábio Souza
+ * data: 26/02/2025
+ */
+
+#include <ESP_I2S.h>
+
+// Definição dos pinos (ajuste conforme sua conexão)
+#define I2S_BCLK  34  // Bit Clock
+#define I2S_LRCLK 35  // Word Select
+#define I2S_DIN   33  // Data In
+
+// Configurações do áudio
+#define SAMPLE_RATE 16000    // 16 kHz de taxa de amostragem
+#define BUFFER_SIZE 1024     // 1024 amostras de 16 bits
+#define BYTE_BUFFER_SIZE BUFFER_SIZE * sizeof(int16_t) // Tamanho em bytes do buffer de áudio (1024 * 2)
+
+I2SClass i2s; // Objeto I2S
+
+void setup() {
+    Serial.begin(115200);
+  
+    // Configurar pinos I2S
+    i2s.setPins(
+    I2S_BCLK,   // BCK
+    I2S_LRCLK,  // WS
+    -1,         // DOUT (não usado)
+    I2S_DIN,    // DIN
+    -1          // MCLK (opcional)
+  );
+  
+  // Inicializar I2S no modo RX
+  if (!i2s.begin(I2S_MODE_STD, SAMPLE_RATE, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO)) {
+    Serial.println("Erro ao inicializar I2S!");
+    while(1); // Loop infinito em caso de falha
+  }
+
+}
+
+void loop() {
+  static int16_t audio_buffer[BUFFER_SIZE]; // Buffer para 1024 amostras de 16 bits
+
+  // Leitura bloqueante (similar ao i2s_read)
+  size_t bytes_read = 0;
+  while(bytes_read < BYTE_BUFFER_SIZE) 
+  {
+    // Lê os dados do microfone e armazena no buffer de áudio (audio_buffer)
+    bytes_read += i2s.readBytes(((char*)audio_buffer) + bytes_read,BYTE_BUFFER_SIZE - bytes_read );
+  }
+
+  // Envia para o Serial Plotter (downsampling 1:8 para facilitar a visualização)
+  for(int i = 0; i < BUFFER_SIZE; i+=8) 
+  {
+    Serial.println(audio_buffer[i]);
+  }
+}
